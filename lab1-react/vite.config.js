d8dfb722da78ed66470e008c01874b45bfbceb89
VoloshchukRostislav <voloshchukrostislav2005@gmail.com>
2026-03-10 22:08:32 +0200

    Final attempt: fixed paths in index.html and config

--- lab1-react/vite.config.js
+++ lab1-react/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lab1-react/', // Переконайтеся, що тут назва репозиторію з косою рисками
+  base: '/lab1-react/', 
 })
